c42e69c66805a03dce8e031777afe644b0af7d77
Tapas Datta <tapash.me@gmail.com>
2016-06-24 17:41:09 +0600

    initial plugin

---
+++ acf-twitter-instagram-posts-picker.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Advanced Custom Fields: Twitter/Instagram Posts Picker
+Plugin URI: http://digitalcreative.asia/
+Description: Search and select videos on YouTube without leaving the page
+Version: 3.0.0
+Author: DigitalCreative
+Author URI: http://digitalcreative.asia/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
