4d900f7c9f00b65f423b60ac90a18d23d645b8e5
Tapas Datta <tapash.me@gmail.com>
2016-06-26 22:36:42 +0600

    modified

--- acf-twitter-posts-picker.php
+++ acf-twitter-posts-picker.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Advanced Custom Fields: Twitter Posts Picker
 Plugin URI: http://digitalcreative.asia/
-Description: Search and select Posts on Twitter/Instagram
+Description: Search and select Posts from Twitter
 Version: 1.0.0
 Author: DigitalCreative
 Author URI: http://digitalcreative.asia/
